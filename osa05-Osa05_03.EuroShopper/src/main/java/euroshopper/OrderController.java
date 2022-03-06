b039147f827ea2d2f820ecc7ce25196bd4171322
smie8 <sami.eriksson@yahoo.fi>
2022-03-06 22:24:07 +0200

    Initial commit.

---
+++ osa05-Osa05_03.EuroShopper/src/main/java/euroshopper/OrderController.java
@@ -0,0 +1,40 @@
+package euroshopper;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OrderController {
+
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @RequestMapping("/orders")
+    public String list(Model model) {
+        model.addAttribute("orders", orderRepository.findAll());
+        return "orders";
+    }
+
+    @PostMapping("/orders")
+    public String order(@RequestParam String name, @RequestParam String address) {
+
+        Order order = new Order();
+        order.setName(name);
+        order.setAddress(address);
+
+
+        orderRepository.save(order);
+
+        return "redirect:/orders";
+    }
+}

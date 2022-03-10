746c509e9c0f6ad56517dc3e15f58c0fdc2bb7a3
smie8 <sami.eriksson@yahoo.fi>
2022-03-10 13:55:44 +0200

    Complete exercise 0304.

---
+++ osa03-Osa03_04.BankTransfer/README.md
@@ -0,0 +1,5 @@
+Tehtäväpohjassa on valmiina yksinkertainen sovellus tilien hallintaan ja tilisiirtojen tekemiseen. Sovelluksen 
+tilisiirtotoiminnallisuudessa on kuitenkin vielä viilattavaa — jos sovellus kaatuu kesken tilisiirron, rahaa voi 
+kadota.
+
+Selvitä minkälaisia korjauksia tilisiirtotoiminnallisuus tarvitsee ja toteuta ne.
--- osa03-Osa03_04.BankTransfer/src/main/java/banktransfer/AccountController.java
+++ osa03-Osa03_04.BankTransfer/src/main/java/banktransfer/AccountController.java
@@ -1,5 +1,6 @@
 package banktransfer;
 
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,16 +20,14 @@ public class AccountController {
         return "index";
     }
 
+    @Transactional
     @PostMapping("/")
     public String transfer(@RequestParam String from, @RequestParam String to, @RequestParam Integer amount) {
         Account accountFrom = this.accountRepository.findByIban(from);
         Account accountTo = this.accountRepository.findByIban(to);
 
         accountFrom.setBalance(accountFrom.getBalance() - amount);
-        accountTo.setBalance(accountTo.getBalance() + amount);
-
-        this.accountRepository.save(accountFrom);
-        this.accountRepository.save(accountTo);        
+        accountTo.setBalance(accountTo.getBalance() + amount);     
         
         return "redirect:/";
     }
